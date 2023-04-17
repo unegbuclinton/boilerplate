79bec96ad85143961357e4c31aa9bdcb300d9171
Clinton <unegbuclinton5@gmail.com>
2023-04-17 18:22:35 +0100

    first commit

--- bin/cli.js
+++ bin/cli.js
@@ -42,7 +42,7 @@ if (process.argv.length < 3) {
 const projectName = process.argv[2];
 const currentPath = process.cwd();
 const projectPath = path.join(currentPath, projectName);
-const git_repo = "https://github.com/unegbuclinton/reacr-tsx-boilerplate.git";
+const git_repo = "https://github.com/unegbuclinton/boilerplate.git";
 
 try {
   fs.mkdirSync(projectPath);
